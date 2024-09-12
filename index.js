4ba8db3af2aa368d0219e9ded32502a74ffce542
mokevip <652831080@qq.com>
2024-09-12 18:28:15 +0800

    修改名称

--- index.js
+++ index.js
@@ -9,7 +9,7 @@ function functionShake(code, functionName) {
 
 module.exports = function rollupRewrite() {
     return {
-        name: 'vite-plugin-rewrite',
+        name: 'vue-event-shake',
         enforce: 'pre', // 标注必须在vite核心插件之前介入
 
         // 处理文件引入 Processing file import
@@ -20,7 +20,7 @@ module.exports = function rollupRewrite() {
             const matchs = code.match(/\.shake=["'](.+?)["']/g)
             if (matchs) {
                 const scriptHeader = code.match(/<script .+?>/)
-                code = code.replace(scriptHeader, `${scriptHeader}\nimport { pluginShakeFunction } from 'vite-vue-shake/shake.js'\n`)
+                code = code.replace(scriptHeader, `${scriptHeader}\nimport { pluginShakeFunction } from 'vue-event-shake/shake.js'\n`)
                 for (const str of matchs) {
                     const functionMatch = str.match(/["'](.+?)["']/)
                     const functionName = functionMatch[1]
